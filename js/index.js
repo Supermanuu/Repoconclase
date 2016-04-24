d326756b72fd73deb8768818a0f65bc66ac6d515
lammers5 <plammers@ucm.es>
2016-04-24 18:27:18 +0200

    Unificado cliente de mensajeria, administrador y index en una unica css. Estas dos css son temporales, luego pasaran a estructura.css y interfaz.css como dice el enunciado
     Cambios para hacer commit:
    	modificado:    administrador.html
    	modificado:    correo.html
    	borrado:       css/admin_estructura.css
    	borrado:       css/admin_interfaz.css
    	borrado:       css/correo_estructura.css
    	borrado:       css/correo_interfaz.css
    	nuevo archivo: css/estructura_Pablo.css
    	borrado:       css/index.css
    	borrado:       css/index_estructura.css
    	borrado:       css/index_interfaz.css
    	nuevo archivo: css/interfaz_Pablo.css
    	modificado:    index.html
    	modificado:    js/index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 
 	var login = function mostrarLogin(){
-  		c = document.getElementById("loginForm2");
+  		c = document.getElementById("index_loginForm2");
   		if (c.style["visibility"] == "hidden" || c.style["visibility"] == "")
 			c.style["visibility"] = "initial";
   		else
@@ -11,17 +11,15 @@ window.onload = function(){
 
  	 var aInfoAlumnos = function(){
 		location.href='detalles_alumnos.html';
-	 }
+	 };
 
 
  	 var aInfoProfes = function(){
 		location.href='detalles_profesores.html';
-	 }
+	 };
 
  	 document.getElementById("login").addEventListener('click', login);
-	
-	 document.getElementById("infoAlumnos").addEventListener('click', aInfoAlumnos);
- 	 document.getElementById("infoProfes").addEventListener('click', aInfoProfes);
-
+	 document.getElementById("index_toAlumnos").addEventListener('click', aInfoAlumnos);
+ 	 document.getElementById("index_toProfes").addEventListener('click', aInfoProfes);
 
 };
