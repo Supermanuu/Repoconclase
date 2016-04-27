9db624c15169a4693dfe306df145f06ee18e2098
lammers5 <plammers@ucm.es>
2016-04-27 18:18:17 +0200

    Muchos retoques en admin, correo e index.
    Añadido nuevas pantallas como escribir un mensaje en correos o
    buscar un usuario en admin - Con AJAX (necesario servidor).
    
    Faltan pequeños retoques para que funcione en moviles tan pequeños
    como el de Fran.
    
     Cambios para hacer commit:
    	nuevo archivo: admin_Usuarios.txt
    	modificado:    administrador.html
    	modificado:    correo.html
    	nuevo archivo: correo_enviar.txt
    	modificado:    css/estructura.css
    	modificado:    css/interfaz.css
    	modificado:    index.html
    	nuevo archivo: js/admin.js
    	nuevo archivo: js/correo.js

---
+++ js/admin.js
@@ -0,0 +1,36 @@
+$(function(){
+
+	$("#admin_gestionUsuarios").click(function(){
+
+		//Peticion al servidor - AJAX
+		$("header.purple > #volver").css ("visibility", "initial");
+		$("#admin_principal").load("admin_Usuarios.txt");
+
+	});
+
+	$("#admin_gestionUsuarios").mouseover(function(){
+		
+		$(this).css ("background-color", "#460A78");
+		
+	});
+
+	$("#admin_gestionUsuarios").mouseleave(function(){
+
+		$(this).css ("background-color", "#6414B4");
+
+	});
+
+	$("header.purple > #volver").click(function(){
+
+		$(location).attr('href', 'administrador.html');
+		$("header.purple > #volver").css ("visibility", "hidden");
+
+	});
+
+	$("#logout").click(function(){
+		
+		$(location).attr('href', 'index.html');
+	
+	});
+
+});
---
+++ js/correo.js
@@ -0,0 +1,17 @@
+$(function(){
+
+	$("#correo_nuevo").click(function(){
+
+		//Peticion al servidor - AJAX
+
+		$("#correo_mostrarMensaje").load("correo_enviar.txt");
+
+	});
+
+	$("#correo_tipoCorreo > select").click(function(){
+		
+		$(location).attr('href', 'correo.html');
+		
+	});
+
+});
