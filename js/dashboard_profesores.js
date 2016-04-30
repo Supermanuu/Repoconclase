2ca59fa8d613955fa613f92382568a74e12df250
danidps95 <pinosanchezdaniel@hotmail.com>
2016-04-30 19:31:07 +0200

    Cambios en Dashboard profes, miembros, ranking
    del dashboard ya estan los enlaces y los shadow
    de miembros cambiado la descripcion
    del ranking aun falta que meta el textarea para la reseña y que unifique
    en el css

--- js/dashboard_profesores.js
+++ js/dashboard_profesores.js
@@ -1,134 +1,57 @@
 $(document).ready (function () {
 
    // Para resaltar los cuadros del dashboard al pasar sobre ellos con el raton
-   $(".dashboard").mouseover (function (){
-      $(this).css ("border-style", "double");
+   $(".claro").mouseover (function (){
       $(this).css ("background-color", "#94AFCB");
    });
-   $(".dashboard").mouseleave (function (){
-      $(this).css ("border-style", "groove");
+   $(".claro").mouseleave (function (){
       $(this).css ("background-color", "#6C8DBC");
    });
+   //para dejar el mismo color al cuadro de info
    $("#tuInfo").mouseleave (function (){
-      $(this).css ("border-style", "groove");
       $(this).css ("background-color", "#376092");
    });
 
  // ACCIONES AL PULSAR SOBRE LOS PANELES 
-   // pulsar en logout
-   $("#buscaUsuario").mousedown (function ()
-   {
-      console.log ("Panel de Buscar Usuarios");
-   });
-
-   $("#buscaAlumno").mousedown (function ()
-   {
-      console.log ("Buscar Alumnos");
-   });
-
-   $("#promocionarse").mousedown (function ()
-   {
-      console.log ("Crear nuevo Anuncio");
-   });
-
-   $("#infoPersonal").mousedown (function ()
-   {
-      console.log ("Panel de Informacion Personal");
-   });
-
-   $("#editarInfo").mousedown (function ()
-   {
-      console.log ("Editar Informacion Personal");
-   });
-
-   $("#opiniones").mousedown (function ()
-   {
-      console.log ("Panel de Opiniones y Valoraciones");
-   });
-
-   $("#verValoraciones").mousedown (function ()
-   {
-      console.log ("Ver tus Valoraciones de Alumnos");
-   });
-
    $("#tusAlumnos").mousedown (function ()
    {
-      console.log ("Panel de Tus Alumnos");
-   });
-
-   $("#gestionaAlumnos").mousedown (function ()
-   {
-      console.log ("Gestionar Alumnos");
-   });
-
-   $("#listaCursos").mousedown (function ()
-   {
-      console.log ("Lista de tus cursos");
-   });
-
-   $("#nuevoCurso").mousedown (function ()
-   {
-      console.log ("crear nuevo curso");
+      $(location).attr('href', 'vista_lista.html?a=pr&b=al');
    });
 
    $("#tusGrupos").mousedown (function ()
    {
-      console.log ("Panel de Tus Grupos");
+      $(location).attr('href', 'vista_lista.html?a=pr&b=gr');
    });
 
-   $("#gestionaGrupos").mousedown (function ()
+   $("#buscaUsuario").mousedown (function ()
    {
-      console.log ("Gestionar Grupos");
-   });
-
-   $("#nuevoGrupo").mousedown (function ()
-   {
-      console.log ("Crear nuevo Grupo");
-   });
-
-   $("#ranking").mousedown (function ()
-   {
-      console.log ("Panel de Ranking");
-   });
-
-   $("#verTop").mousedown (function ()
-   {
-      console.log ("Ver TOP 10 de Profesores");
+      $(location).attr('href', 'buscador.html');
    });
 
    $("#correo").mousedown (function ()
    {
-      console.log ("Panel de Correo");
+      $(location).attr('href', 'correo.html');
    });
 
-   $("#redactarMensaje").mousedown (function ()
+
+   $("#infoPersonal").mousedown (function ()
    {
-      console.log ("Redactar Nuevo Mensaje");
+      $(location).attr('href', 'editar_usuario.html');
    });
 
-   $("#entradaMensajes").mousedown (function ()
+   $("#valoraciones").mousedown (function ()
    {
-      console.log ("Ver Buzon de Entrada de tus Mensajes");
+      $(location).attr('href', 'estadisticas.html');
    });
 
-   $("#estadisticas").mousedown (function ()
+   $("#ranking").mousedown (function ()
    {
-      console.log ("Panel de Estadisticas");
-   });
-
-   $("#estadsAlumnos").mousedown (function ()
-   {
-      console.log ("Ver Estadisticas de Tus Alumnos");
-   });
-
-   $("#estadsGrupos").mousedown (function ()
-   {
-      console.log ("Ver Estadisticas de Tus Grupos");
+      $(location).attr('href', 'ranking.html');
    });
 
    $("#video").mousedown (function ()
    {
-      console.log ("Ver video de uso de la web");
+      $(location).attr('href', ' ');
    });
 
 });
