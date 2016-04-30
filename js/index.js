3b9176301ff38780d4f261f8dd84fb1540532c1d
Fran <frburrue@ucm.es>
2016-04-30 18:09:44 +0200

    Revisar all.html

--- js/index.js
+++ js/index.js
@@ -9,6 +9,11 @@ window.onload = function() {
 		location.href='detalles_profesores.html';
 	 };
 
+	 var general = function(){
+		location.href='all.html';
+	 };
+
+	 document.getElementById("general").addEventListener('click', general);
 	 document.getElementById("index_toAlumnos").addEventListener('click', aInfoAlumnos);
  	 document.getElementById("index_toProfes").addEventListener('click', aInfoProfes);
 
