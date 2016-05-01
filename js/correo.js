a67105f66f987d333c9fde0fd566d4b9bf3fd4f0
danidps95 <pinosanchezdaniel@hotmail.com>
2016-05-01 12:01:11 +0200

    Sustitucion del Repo por los archivos de la entrega

--- js/correo.js
+++ js/correo.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
 
+   tokens = location.search.substr(1).split ("&");
+
+   if (tokens [0] == "a=al")
+   {
+       //Eliminamos color por defecto
+      $("header").removeClass ("blue");
+      $("#logout").removeClass ("blue");
+     
+      $("footer").removeClass ("blue");
+      $("#correo_tipoCorreo > select").removeClass ("blue");
+      $("#correo_nuevo").removeClass ("blue");
+
+      // Añadimos la clase del usuario
+      $("header").addClass ("green");
+      $("#logout").addClass ("green");
+      $("footer").addClass ("green");
+      
+      // Cambiamos el color del buscador
+      $("#correo_tipoCorreo > select").addClass ("green");
+      $("#correo_nuevo").addClass ("green");
+      $("#correo_correos > #correo_detallesCorreo").css("background-color", "#6AC46E");
+      $("#correo_correos > #correo_detallesCorreo").css("box-shadow", "5px 5px 5px #118E56");
+      $("#correo_mostrarMensaje").css("background-color", "#6AC46E");
+      $("#correo_mostrarMensaje").css("box-shadow", "5px 5px 5px #118E56");
+      $(".correo_linea").css("border-color", "#118E56");
+
+   };
+
 	$("#correo_nuevo").click(function(){
 
 		//Peticion al servidor - AJAX
@@ -9,9 +37,10 @@ $(document).ready(function() {
 	});
 
 	$("#correo_tipoCorreo > select").click(function(){
-		
-		$(location).attr('href', 'correo.html');
+		var auxiliar = "correo.html?" + tokens[0];
+		$(location).attr('href', auxiliar);
 		
 	});
 
 });
+
