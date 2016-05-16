f46221a2f21b205fea8b3e869f8b6c6741509e83
Manuel Pascual López <manupa01@ucm.es>
2016-05-16 17:10:04 +0200

    Index alumnos finalizado!!
    Ya se accede a todas las bases de datos

--- index_alumnos.php
+++ index_alumnos.php
@@ -15,8 +15,8 @@
    else if ($_SESSION ["type"] != "alumno")
       header('Location: index.php');
    
-   /* Desde aqui, esta claro que el login es correcto, asique rellenamos la pagina web con la info del alumno */
    
+   /* Desde aqui, esta claro que el login es correcto, asique rellenamos la pagina web con la info del alumno */
    
    // Conectamos con la base de datos
    $conexion = new mysqli ('localhost', 'profesores', 'profesConEstilo', 'profesoresConClase');
@@ -67,6 +67,7 @@
    if ($perfil != "alumno")
       header('Location: index.php');
 
+   
    // El nombre y los apellidos del alumno estan en nombre, apellido1, apellido2
    
    
@@ -119,9 +120,62 @@
       $clases[] = $nombre_asignatura . " " . $nivel . " " . $curso;
       $j++;
    }
+   if ($i == 0)
+      $clases[] = "Puede que me venga bien alguna..."; 
    
    
    // Las tres primeras clases de este alumno estan en el array clases, si hay
+   
+   
+   // Preparamos la query que vamos a ejecutar: Obtenemos la informacion de un grupo seleccionado
+   $sentencia->free_result ();
+   if (! ($sentencia = $conexion->prepare ("SELECT nombre_curso, hora_ini, dias_semana FROM cursos WHERE id_curso IN (SELECT id_curso FROM cursos_seleccionados WHERE id_alumno = (?));")))
+      echo "ERROR: PREPARE (5): " . $conexion->error;
+   // Asociamos la variable a la query: Es el id del usuario logeado
+   if (!$sentencia->bind_param ("i", $_SESSION ["id_user"])) 
+      echo "ERROR: BIND PARAM (5): " . $conexion->error;
+   // Ejecutamos la query en la BD
+   if (!$sentencia->execute ())
+      echo "ERROR: EXECUTE (5): " . $conexion->error;
+   // Vinculamos la salida a otras variables: Esperamos la info de la clase
+   if (!$sentencia->bind_result ($nombre_curso, $hora_ini, $dias_semana))
+      echo "ERROR: BIND RESULT (5): " . $conexion->error;
+   // Recorremos las 3 primeras filas, si hay
+   $i = 0;
+   while ($sentencia->fetch () && i < 3)
+   {
+      $cursos[] = $nombre_curso . " a las " . $hora_ini . " - " . $dias_semana;
+      $i++;
+   }
+   if ($i == 0)
+      $cursos[] = "¡Estaría bien apuntarse a uno!"; 
+   
+   
+   // Los tres primeros cursos de este alumno estan en el array cursos, si hay
+   
+   
+   // Preparamos la query que vamos a ejecutar: Obtenemos la informacion de un grupo seleccionado
+   $sentencia->free_result ();
+   if (! ($sentencia = $conexion->prepare ("SELECT user FROM usuarios WHERE idUser IN (SELECT id_profesor FROM profes_seleccionados WHERE id_alumno = (?));")))
+      echo "ERROR: PREPARE (5): " . $conexion->error;
+   // Asociamos la variable a la query: Es el id del usuario logeado
+   if (!$sentencia->bind_param ("i", $_SESSION ["id_user"])) 
+      echo "ERROR: BIND PARAM (5): " . $conexion->error;
+   // Ejecutamos la query en la BD
+   if (!$sentencia->execute ())
+      echo "ERROR: EXECUTE (5): " . $conexion->error;
+   // Vinculamos la salida a otras variables: Esperamos la info de la clase
+   if (!$sentencia->bind_result ($user_profesor))
+      echo "ERROR: BIND RESULT (5): " . $conexion->error;
+   // Recorremos las 3 primeras filas, si hay
+   $i = 0;
+   while ($sentencia->fetch () && i < 3)
+   {
+      $profesores[] = $user_profesor;
+      $i++;
+   }
+   if ($i == 0)
+      $profesores[] = "¿Ningun profesor interesante?"; 
    
    
    // Cerramos conexion con la base de datos
@@ -130,10 +184,12 @@
       echo "ERROR: CLOSE: " . $conexion->error;
 ?>
 
-<html>
+<html lang="es-ES">
 	<head>
 		<title>Profesores con clase</title>
 		<meta charset="utf-8"/>
+      <meta name="author" content="Sistemas web 15/16">
+      <meta name="description" content="Pagina principal de los alumnos">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
       <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
       <link rel="stylesheet" type="text/css" href="css/reset.css"/>
@@ -159,9 +215,9 @@
                <div id="mis_profesores" class="pulsable">
                   <h1 class="pulsable">Mis profesores</h1>
                   <!--p id ="mis_profesores_1" class="pulsable">Rafael del Vado López</p-->
-                  <p id ="mis_profesores_1" class="pulsable"> <?php if ($profesores [0] != null) echo $profesores [0]; else echo '<br>'; ?> </p>
-                  <p id ="mis_profesores_2" class="pulsable"> <?php if ($profesores [1] != null) echo $profesores [1]; else echo '<br>'; ?> </p>
-                  <p id ="mis_profesores_3" class="pulsable"> <?php if ($profesores [2] != null) echo $profesores [2]; else echo '<br>'; ?> </p>
+                  <p id ="mis_profesores_1" class="pulsable" title="<?php echo $profesores [0]; ?>"> <?php if ($profesores [0] != null) echo $profesores [0]; else echo '<br>'; ?> </p>
+                  <p id ="mis_profesores_2" class="pulsable" title="<?php echo $profesores [1]; ?>"> <?php if ($profesores [1] != null) echo $profesores [1]; else echo '<br>'; ?> </p>
+                  <p id ="mis_profesores_3" class="pulsable" title="<?php echo $profesores [2]; ?>"> <?php if ($profesores [2] != null) echo $profesores [2]; else echo '<br>'; ?> </p>
                </div>
                <div id="correo_alumno" class="pulsable">
                   <h1 class="pulsable">Correo</h1>
@@ -171,9 +227,9 @@
             <div id="bloque_2">
                <div id="mis_cursos" class="pulsable">
                   <h1 class="pulsable">Mis cursos</h1>
-                  <p id ="mis_grupos_1" class="pulsable"> <?php if ($cursos [0] != null) echo $cursos [0]; else echo '<br>'; ?> </p>
-                  <p id ="mis_grupos_2" class="pulsable"> <?php if ($cursos [1] != null) echo $cursos [1]; else echo '<br>'; ?> </p>
-                  <p id ="mis_grupos_3" class="pulsable"> <?php if ($cursos [2] != null) echo $cursos [2]; else echo '<br>'; ?> </p>
+                  <p id ="mis_grupos_1" class="pulsable" title="<?php echo $cursos [0]; ?>"> <?php if ($cursos [0] != null) echo $cursos [0]; else echo '<br>'; ?> </p>
+                  <p id ="mis_grupos_2" class="pulsable" title="<?php echo $cursos [1]; ?>"> <?php if ($cursos [1] != null) echo $cursos [1]; else echo '<br>'; ?> </p>
+                  <p id ="mis_grupos_3" class="pulsable" title="<?php echo $cursos [2]; ?>"> <?php if ($cursos [2] != null) echo $cursos [2]; else echo '<br>'; ?> </p>
                </div>
                <div id="top_10_profesores" class="pulsable">
                   <h1 class="pulsable">Top 10 profesores</h1>
@@ -184,16 +240,16 @@
                <div id="mis_clases" class="pulsable">
                   <h1 class="pulsable">Mis clases</h1>
                   <div id="mis_clases_1" class="pulsable">
-                     <p id ="mis_clases_1_nombre" class="pulsable">  <?php if ($clases [0] != null) echo $clases [0]; else echo '<br>'; ?> </p>
-                     <p id ="mis_clases_1_horario" class="pulsable"> <?php if ($horas_clases [0] != null) echo $horas_clases [0]; else echo '<br>'; ?> </p>
+                     <p id ="mis_clases_1_nombre" class="pulsable" title="<?php echo $clases [0]; ?>">  <?php if ($clases [0] != null) echo $clases [0]; else echo '<br>'; ?> </p>
+                     <p id ="mis_clases_1_horario" class="pulsable" title="<?php echo $horas_clases [0]; ?>"> <?php if ($horas_clases [0] != null) echo $horas_clases [0]; else echo '<br>'; ?> </p>
                   </div>
                   <div id="mis_clases_2" class="pulsable">
-                     <p id ="mis_clases_2_nombre" class="pulsable">  <?php if ($clases [1] != null) echo $clases [1]; else echo '<br>'; ?> </p>
-                     <p id ="mis_clases_2_horario" class="pulsable"> <?php if ($horas_clases [1] != null) echo $horas_clases [1]; else echo '<br>'; ?> </p>
+                     <p id ="mis_clases_2_nombre" class="pulsable" title="<?php echo $clases [1]; ?>">  <?php if ($clases [1] != null) echo $clases [1]; else echo '<br>'; ?> </p>
+                     <p id ="mis_clases_2_horario" class="pulsable" title="<?php echo $horas_clases [1]; ?>"> <?php if ($horas_clases [1] != null) echo $horas_clases [1]; else echo '<br>'; ?> </p>
                   </div>
                   <div id="mis_clases_3" class="pulsable">
-                     <p id ="mis_clases_3_nombre" class="pulsable">  <?php if ($clases [2] != null) echo $clases [2]; else echo '<br>'; ?> </p>
-                     <p id ="mis_clases_3_horario" class="pulsable"> <?php if ($horas_clases [2] != null) echo $horas_clases [2]; else echo '<br>'; ?> </p>
+                     <p id ="mis_clases_3_nombre" class="pulsable" title="<?php echo $clases [2]; ?>">  <?php if ($clases [2] != null) echo $clases [2]; else echo '<br>'; ?> </p>
+                     <p id ="mis_clases_3_horario" class="pulsable" title="<?php echo $horas_clases [2]; ?>"> <?php if ($horas_clases [2] != null) echo $horas_clases [2]; else echo '<br>'; ?> </p>
                   </div>
                </div>
                <div id="acciones_no_tan_rapidas">
