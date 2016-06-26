6fd09d839fa9d3c914f1cccd4186f18a3c87d0d1
fburrue <frburrue@ucm.es>
2016-06-26 10:09:49 +0200

    Dopado admin_elimina.php

--- php/admin_elimina.php
+++ php/admin_elimina.php
@@ -1,19 +1,63 @@
 <?php
+    
+    function borrarDirectorio($carpeta) {
 
-    $user = $_REQUEST["user"];
+        foreach(glob($carpeta . "*") as $archivos_carpeta) {
+ 
+            if (is_dir($archivos_carpeta)) {
+                eliminarDir($archivos_carpeta);
+            }
+            else {
+                unlink($archivos_carpeta);
+            }
+        }
+ 
+        rmdir($carpeta);
 
-    //Conectar con la base de datos y eliminar usuario
-
-    $mysqli = new mysqli('localhost', 'profesores', 'profesConEstilo', 'profesoresConClase');
-    if (mysqli_connect_errno()){
-	echo '<h1>Error interno</h1>';
-	exit();
     }
 
-    $consulta = "DELETE from usuarios where user='$user'";
-    $mysqli->query($consulta) or die($mysqli->error);
+    session_start();
 
-    $mysqli->close();
+    if (!isset($_SESSION["login"]) || $_SESSION["login"] == false) {
+       header('Location: ../index.php');
+    }
 
-    header('Location: ../administrador_gestor.php');
+    if(!isset($_COOKIE[$_SESSION["type"]])) {
+      session_destroy();
+      header('Location: ../sesion_expirada.php');
+    }
+    else
+      setcookie($_SESSION["type"], $_SESSION["id_user"], time() + 60*5, "/");
+
+    if (isset($_SESSION["login"]) && $_SESSION["login"] == true){
+
+        if (!isset($_REQUEST["user"]))
+            header('Location: ../administrador_gestor.php');
+        else
+            $user = $_REQUEST["user"];
+
+        $mysqli = new mysqli ('localhost', 'profesores','profesConEstilo','profesoresConClase');
+        if (!mysqli_connect_errno()) {
+
+            $query = "SELECT * FROM usuarios where user = '$user'";
+            $resultado = $mysqli->query($query);
+            $usuario = $resultado->fetch_assoc();
+            $id = $usuario["id_user"];
+
+            $query = "SELECT * FROM folders where id = '$id'";
+            $resultado = $mysqli->query($query);
+            $usuario = $resultado->fetch_assoc();
+            $directorio = '/var/www/html'.$usuario["folder"];
+            borrarDirectorio($directorio);
+
+            $query = "DELETE FROM usuarios WHERE idUser = '$id'";
+            $resultado = $mysqli->query($query);
+            $mysqli->close();
+            header('Location: ../administrador.php');
+
+        }
+    }
+    else
+        header('Location: ../index.php');
+
 ?>
