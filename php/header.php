3cac669acdcdd1584fec6e62fb48aa1d1e95c9f2
danidps95 <pinosanchezdaniel@hotmail.com>
2016-06-26 21:55:40 +0200

    botones header a full

--- php/header.php
+++ php/header.php
@@ -13,10 +13,10 @@
      echo '<header class="green">';
      echo '<div id=pcc_verde></div>';
      echo '<div id=pcc_botones>';
-     echo '<button class="green" id="logout">Logout</button>';
-	 if($_SESSION["volverIndex"] == 1){
+     if($_SESSION["volverIndex"] == 1){
         echo '<button class="green" id="volver">Mi Sitio</button>'; //en common.js proceso el click 
      }
+     echo '<button class="green" id="logout">Logout</button>';
      echo '</div>';
      echo '</header>';
   }
@@ -24,11 +24,10 @@
      echo '<header class="blue">';
      echo '<div id=pcc_azul></div>';
      echo '<div id=pcc_botones>';
-     echo '<button class="blue" id="logout">Logout</button>';
-
      if($_SESSION["volverIndex"] == 1){
         echo '<button class="blue" id="volver">Mi Sitio</button>';
      }
+     echo '<button class="blue" id="logout">Logout</button>';
      echo '</div>';
      echo '</header>';
   }
@@ -36,11 +35,10 @@
      echo '<header class="purple">';
      echo '<div id=pcc_morado></div>';
      echo '<div id=pcc_botones>';
-     echo '<button class="purple" id="logout">Logout</button>';
-     
      if($_SESSION["volverIndex"] == 1){
         echo '<button class="purple" id="volver">Mi Sitio</button>';
      }
+     echo '<button class="purple" id="logout">Logout</button>';
      echo '</div>';
      echo '</header>';
   }
